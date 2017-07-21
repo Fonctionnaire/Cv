4bc18bb21ba964910c1159eb48490fd1f7f56ee4
Fonctionnaire <thibaut.gruffy@gmail.com>
2017-07-21 16:11:40 +0200

    Ajout des réalisations. Début du form de contact

---
+++ src/AppBundle/Form/Type/ContactType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContactType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array(
+                'constraints' => array(new NotBlank(),
+                    new Length(array(
+                        'min' => 2,
+                        'max' => 40,
+                        'minMessage' => 'Nom invalide',
+                        'maxMessage' => 'Le nom ne peut dépasser 40 caractères'
+                    )),
+                )))
+            ->add('firstName', TextType::class, array(
+                'constraints' => array( new NotBlank(),
+                    new Length(array(
+                        'min' => 2,
+                        'max' => 40,
+                        'minMessage' => 'Prénom invalide',
+                        'maxMessage' => 'Le prénom ne peut dépasser 40 caractères'
+                    )),
+                )))
+            ->add('societe', TextType::class)
+            ->add('mail', EmailType::class, array(
+                'constraints' => array( new Email(),
+                    new NotBlank(),
+                )))
+            ->add('message', TextareaType::class, array(
+                'constraints' => array( new NotBlank(),
+                    new Length(array(
+                        'min' => 2,
+                        'max' => 1205,
+                        'minMessage' => 'Votre message doit comporter au moins 2 caractères',
+                        'maxMessage' => 'Votre message ne peut dépasser 1200 caractères'
+                    )),
+                )))
+            ->add('envoyer', SubmitType::class)
+            ->getForm();
+    }
+
+}
---
+++ web/javascript/modal.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%' // Ending top style attribute
+
+    });
+});
+
